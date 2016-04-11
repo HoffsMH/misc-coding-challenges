9988f4584a32da802399f9ffeaa80802727e2685
HoffsMH <hecker.matthew@gmail.com>
2016-04-11 17:37:40 -0600

    finishes find_rotation2

--- rotation-search/main.rb
+++ rotation-search/main.rb
@@ -13,6 +13,8 @@ words = [
     'karpatka',
     'othellolagkage',
 ]
+words2 = [ 'k','v','a','b','c','d','e','g','i' ]
+words3 = ["j", 'k','l','m','n','o','v','w','x','y','z','a','b','c','d','e','g','i' ]
 
 def find_rotation1(word_arr)
   rotation_index = nil
@@ -24,6 +26,21 @@ def find_rotation1(word_arr)
     end
   end
   rotation_index
+end
+
+
+def find_rotation2(word_arr, index=0)
+  middle_index = (word_arr.length - 1) / 2
+  middle_element = word_arr[middle_index]
+  if middle_element < word_arr.first
+    if (word_arr.length == 2) || (word_arr[middle_index - 1] > middle_element)
+      return index + middle_index
+    else
+      find_rotation2(word_arr[0..middle_index], index )
+    end
+  else
+    find_rotation2(word_arr[middle_index..-1], (index + middle_index))
+  end
 end
 
 binding.pry
